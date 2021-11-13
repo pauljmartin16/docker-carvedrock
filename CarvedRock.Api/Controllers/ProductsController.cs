866a78ba68a1cbc2763c6621cbbcfe4a51f08894
pauljmartin16 <pauljmartin16@gmail.com>
2021-11-12 20:27:59 -0500

    Add files via upload

---
+++ CarvedRock.Api/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using CarvedRock.Api.ApiModels;
+using CarvedRock.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System.Collections.Generic;
+
+namespace CarvedRock.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductLogic _productLogic;
+
+        public ProductsController(IProductLogic productLogic)
+        {
+            _productLogic = productLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts(string category = "all")
+        {
+            //_logger.LogInformation("Starting controller action GetProducts for {category}", category);
+
+            // example of static Serilog Logger
+            Log.Information("Starting controller action GetProducts for {category}", category);
+            return _productLogic.GetProductsForCategory(category);
+        }
+    }
+}
